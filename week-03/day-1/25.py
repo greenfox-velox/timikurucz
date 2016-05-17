0021d8cf38e88eb3f734dba41655769c1c10e6a6
timikurucz <kurucztm@gmail.com>
2016-05-17 16:48:01 +0200

    Add 21-30 excercises

---
+++ week-03/day-1/25.py
@@ -0,0 +1,11 @@
+x = 'cheese'
+# Tell if the x has even or odd number of
+# characters with a True for even and
+# false False output otherwise
+
+char_num = len(x)
+
+if char_num % 2 == 0:
+    print ("True")
+else:
+    print ("False")
