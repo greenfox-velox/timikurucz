4a2a1ed1aa897ba1c73dd3ab7d4d4c18cfb636d6
timikurucz <kurucztm@gmail.com>
2016-05-23 21:01:18 +0200

    Changed variables' names

--- week-04/day-1/1-pip-io-modules/01_io/crypto_3revorder.py
+++ week-04/day-1/1-pip-io-modules/01_io/crypto_3revorder.py
@@ -4,8 +4,8 @@ def decrypt(file_name):
     f = open(file_name)
     text_list = f.readlines()
     rev_list = reversed(text_list)
-    new = ''
+    new_string = ''
     for item in rev_list:
-        new += item
+        new_string += item
     f.close()
-    return new
+    return new_string
