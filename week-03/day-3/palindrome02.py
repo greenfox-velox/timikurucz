136bb75321b47cc0f7435de07aea4ae946af0e3c
timikurucz <kurucztm@gmail.com>
2016-05-24 21:58:49 +0200

    Refactor for readability

--- week-03/day-3/palindrome02.py
+++ week-03/day-3/palindrome02.py
@@ -1,35 +1,26 @@
 # Create a function that searches for all the palindromes in a string that are at least than 3 characters, and returns a list with the found palindromes. Example:
-#
 # output = search_palindromes('dog goat dad duck doodle never')
 # print(output) # it prints: ['dad', 'dood', 'eve']
 
-words = 'dog goat dad duck doodle never'
-new = 'mom and dad sit in a kayak'
-
-# word = 'dog goat dad duck doodle never'
-# length = len(word)
-
-# for i in range (0, (length-1)):
-#     for x in range ((i+3), (length+1)):
-#         if (word[i:x]) == (word[i:x])[::-1]:
-#             print(word[i:x])
-#
-# def is_there_palindromes(input):
-#     length = len(input)
-#     for i in range (0, (length-1)):
-#         for x in range ((i+3), (length+1)):
-#             if (input[i:x]) == (input[i:x])[::-1]:
-#                 print(input[i:x])
-#
-# is_there_palindromes(word)
-
-sentence = 'mom and dad sit in a kayak'
 def is_there_palindromes(input):
     length = len(input)
     palindromes_list = []
     for i in range (0, (length-1)):
         for x in range ((i+3), (length+1)):
-            if input[i:x] == input[i:x][::-1]:
+            if (input[i:x]) == (input[i:x])[::-1]:
                 palindromes_list.append(input[i:x])
     return palindromes_list
-print(is_there_palindromes(sentence))
+
+print(is_there_palindromes('dog goat dad duck doodle never'))
+
+
+# sentence = 'mom and dad sit in a kayak'
+# def is_there_palindromes(input):
+#     length = len(input)
+#     palindromes_list = []
+#     for i in range (0, (length-1)):
+#         for x in range ((i+3), (length+1)):
+#             if input[i:x] == input[i:x][::-1]:
+#                 palindromes_list.append(input[i:x])
+#     return palindromes_list
+# print(is_there_palindromes(sentence))
