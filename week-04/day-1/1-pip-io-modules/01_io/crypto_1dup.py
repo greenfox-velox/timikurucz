c09e40bc5c1292f9bdd077a6ff19cb5cca047f56
timikurucz <kurucztm@gmail.com>
2016-05-24 21:29:29 +0200

    Refactor: use readlines()

--- week-04/day-1/1-pip-io-modules/01_io/crypto_1dup.py
+++ week-04/day-1/1-pip-io-modules/01_io/crypto_1dup.py
@@ -1,10 +1,21 @@
 # Create a method that decrypts the texts/duplicated_chars.txt
 
+# def decrypt(file_name):
+#     f = open(file_name)
+#     new = ''
+#     for line in f:
+#         for i in range(0, len(line), 2):
+#             new += line[i]
+#     f.close()
+#     return new
+
+
 def decrypt(file_name):
     f = open(file_name)
-    new = ''
-    for line in f:
+    input_text_list = f.readlines()
+    output_text = ''
+    for line in input_text_list:
         for i in range(0, len(line), 2):
-            new += line[i]
+            output_text += line[i]
     f.close()
-    return new
+    return output_text
