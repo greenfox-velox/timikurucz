c00d51c549bff2299e00ddac15ebce4b66a1368b
timikurucz <kurucztm@gmail.com>
2016-05-26 19:53:02 +0200

    Added 11th exercise

---
+++ week-04/day-4/11.py
@@ -0,0 +1,24 @@
+from tkinter import*
+root = Tk()
+
+canvas_size = 300
+
+canvas = Canvas(root, width = canvas_size, height = canvas_size, bg='yellow')
+canvas.pack()
+
+def fractal(x, y, size):
+    canvas.create_rectangle(x+size, y, x+2*size, y+size)
+    canvas.create_rectangle(x+2*size, y+size, x+3*size, y+2*size)
+    canvas.create_rectangle(x, y+size, x+size, y+2*size)
+    canvas.create_rectangle(x+size, y+2*size, x+2*size, y+3*size)
+    if size < 5:
+        pass
+    else:
+        fractal(x+size, y, size//3)
+        fractal(x, y+size, size//3)
+        fractal(x+size*2, y+size, size//3)
+        fractal(x+size, y+2*size, size//3)
+
+fractal(0,0,100)
+
+root.mainloop()
